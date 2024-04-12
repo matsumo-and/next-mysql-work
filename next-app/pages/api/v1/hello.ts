569d422ab834cbf5ca3786caeb0057c10aed0eef
matsumo-and <yh134.toisanda@gmail.com>
2024-04-13 03:43:34 +0900

    [fix]prisma_setting

--- next-app/pages/api/v1/hello.ts
+++ next-app/pages/api/v1/hello.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import prisma from '@/infrastructure/prisma';
+import prisma, { User } from '@/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
@@ -7,6 +7,6 @@ type Data = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const user = await prisma.user.findFirst();
+  const user: User | null = await prisma.user.findFirst();
   res.status(200).json({ name: user?.name ?? 'John Doe' });
 }
